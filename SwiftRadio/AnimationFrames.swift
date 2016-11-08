298af07a1da691fe30a986f25e5e3e293b04c6f1
Khululekani Mkhonza <khululekanim@cpwonlinesolutions.com>
2016-11-08 22:26:56 +0000

    Initial Commit

---
+++ SwiftRadio/AnimationFrames.swift
@@ -0,0 +1,31 @@
+//
+//  AnimationFrames.swift
+//  Swift Radio
+//
+//  Created by Thebe Media on 7/2/15.
+//  Copyright (c) 2015 thebemedia.com. All rights reserved.
+//
+
+import UIKit
+
+class AnimationFrames {
+    
+    class func createFrames() -> [UIImage] {
+    
+        // Setup "Now Playing" Animation Bars
+        var animationFrames = [UIImage]()
+        for i in 0...3 {
+            if let image = UIImage(named: "NowPlayingBars-\(i)") {
+                animationFrames.append(image)
+            }
+        }
+        
+        for i in stride(from: 2, to: 0, by: -1) {
+            if let image = UIImage(named: "NowPlayingBars-\(i)") {
+                animationFrames.append(image)
+            }
+        }
+        return animationFrames
+    }
+
+}
---
+++ SwiftRadio/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Swift Radio
+//
+//  Created by Thebe Media on 7/13/15.
+//  Copyright (c) 2015 thebemedia.com. All rights reserved.
+//
+
+import MediaPlayer
+
+//*****************************************************************
+// This is a singleton struct using Swift
+//*****************************************************************
+
+struct Player {
+    static let radio = MPMoviePlayerController()
+}
